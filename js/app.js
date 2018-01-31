66d3c8e3c23f111d81d18383f7c64491b9497b8e
Andre Bomark <bomarkandre@gmail.com>
2018-01-31 20:35:49 +0100

    style: indent, whitespace

--- js/app.js
+++ js/app.js
@@ -50,110 +50,110 @@ class Enemy {
   */
   collide(playerObj) {
     if (
-    playerObj.y + 131 >= this.y + 90
-    && playerObj.x + 25 <= this.x + 88
-    && playerObj.y + 73 <= this.y + 135
-    && playerObj.x + 76 >= this.x + 11) {
-      playerObj.x = 202.5;
-      playerObj.y = 483;
-      playerObj.wins = 0;
-      document.querySelector('.wincount').innerHTML = "Wins: " + playerObj.wins;
+      playerObj.y + 131 >= this.y + 90
+      && playerObj.x + 25 <= this.x + 88
+      && playerObj.y + 73 <= this.y + 135
+      && playerObj.x + 76 >= this.x + 11) {
+        playerObj.x = 202.5;
+        playerObj.y = 483;
+        playerObj.wins = 0;
+        document.querySelector('.wincount').innerHTML = "Wins: " + playerObj.wins;
       }
     }
-}
-
-
-/**
-* @description Represent the player.
-* @constructor
-*/
-class Player {
-  constructor() {
-    this.x = 202.5;
-    this.y = 483;
-    this.speed = 100;
-    this.maxDistanceY = 383;
-    this.maxDistanceX = 402.5;
-    this.minDistanceX = 2.5;
-    this.sprite = 'images/char-boy.png';
-    this.wins = 0;
   }
 
-  /**
-  * @description Stops player if trying to walk past border
-  */
-  distanceRestrictions() {
-    (this.x > this.maxDistanceX) ? this.x = this.maxDistanceX : this.x;
-    (this.x < this.minDistanceX) ? this.x = this.minDistanceX : this.x;
-    (this.y > this.maxDistanceY) ? this.y = this.maxDistanceY : this.y;
-  }
 
   /**
-  * @description Update's player data if winning condition is met
+  * @description Represent the player.
+  * @constructor
   */
-  updateIfWin() {
-    if (this.y + 63 <= 0) {
+  class Player {
+    constructor() {
       this.x = 202.5;
       this.y = 483;
-      this.wins += 1;
-      document.querySelector('.wincount').innerHTML = "Wins: " + this.wins;
+      this.speed = 100;
+      this.maxDistanceY = 383;
+      this.maxDistanceX = 402.5;
+      this.minDistanceX = 2.5;
+      this.sprite = 'images/char-boy.png';
+      this.wins = 0;
     }
-  }
 
-  /**
-  * @description Update the player's position, distance restrictions and winning conditions
-  */
-  update() {
-    this.distanceRestrictions();
-    this.updateIfWin();
-  }
+    /**
+    * @description Stops player if trying to walk past border
+    */
+    distanceRestrictions() {
+      (this.x > this.maxDistanceX) ? this.x = this.maxDistanceX : this.x;
+      (this.x < this.minDistanceX) ? this.x = this.minDistanceX : this.x;
+      (this.y > this.maxDistanceY) ? this.y = this.maxDistanceY : this.y;
+    }
 
-  /**
-  * @description Draw the player on the screen
-  */
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
+    /**
+    * @description Update's player data if winning condition is met
+    */
+    updateIfWin() {
+      if (this.y + 63 <= 0) {
+        this.x = 202.5;
+        this.y = 483;
+        this.wins += 1;
+        document.querySelector('.wincount').innerHTML = "Wins: " + this.wins;
+      }
+    }
 
-  /**
-  * @description Handler for key inputs
-  * @param {string} key - Recorded keystrokes for player movements
-  */
-  handleInput(key) {
-    console.log(key);
-    switch(key) {
-      case 'left':
+    /**
+    * @description Update the player's position, distance restrictions and winning conditions
+    */
+    update() {
+      this.distanceRestrictions();
+      this.updateIfWin();
+    }
+
+    /**
+    * @description Draw the player on the screen
+    */
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    /**
+    * @description Handler for key inputs
+    * @param {string} key - Recorded keystrokes for player movements
+    */
+    handleInput(key) {
+      console.log(key);
+      switch(key) {
+        case 'left':
         this.x -= this.speed;
         break;
-      case 'up':
+        case 'up':
         this.y -= this.speed - 20;
         break;
-      case 'right':
+        case 'right':
         this.x += this.speed;
         break;
-      case 'down':
+        case 'down':
         this.y += this.speed - 20;
         break;
+      }
     }
   }
-}
 
-const bug1 = new Enemy();
-const bug2 = new Enemy();
-const bug3 = new Enemy();
+  const bug1 = new Enemy();
+  const bug2 = new Enemy();
+  const bug3 = new Enemy();
 
-const allEnemies = [bug1, bug2, bug3];
-const player = new Player();
+  const allEnemies = [bug1, bug2, bug3];
+  const player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+  // This listens for key presses and sends the keys to your
+  // Player.handleInput() method. You don't need to modify this.
+  document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+  });
